feae02f99514d1f64f20350bed82695aab3ad0e0
Zherro <cleiton.ti@outlook.com>
2021-05-09 17:49:02 -0300

    melhorando switch case

--- src/components/FormRegistry.jsx
+++ src/components/FormRegistry.jsx
@@ -1,37 +1,32 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
 import { Typography } from '@material-ui/core';
 
-function FormRegistry({validarCPF, aoEnviar}) {
+function FormRegistry({ validarCPF, aoEnviar }) {
 
     const [etapaAtual, setEtapaAtual] = useState(0);
     const [dadosColetados, setDados] = useState({});
 
-    function formularioAtual(etapa) {
-        switch(etapa) {
-           case 0:
-                return <DadosUsuario aoEnviar={proximo} />;
-            case 1:
-                return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-            case 2:
-                return <DadosEntrega aoEnviar={aoEnviar}/>;
-            default:
-                return <Typography>Erro ao selecionar formulario</Typography>
-    
-        }
-    }
+    useEffect(() => {
+        console.log(dadosColetados);
+    })
 
+    const formularios = [
+        <DadosUsuario aoEnviar={coletarDados} />,
+        <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+        <DadosEntrega aoEnviar={coletarDados} />,
+    ];
+
+    function coletarDados(dados) {
+        setDados({ ...dadosColetados, ...dados });
+        proximo();
+    }
     function proximo() {
         setEtapaAtual(etapaAtual + 1);
     }
-
-    return (
-        <Fragment>
-            { formularioAtual(etapaAtual) }
-        </Fragment>
-    );
+    return <>{formularios[etapaAtual]}</>;
 }
 
 export default FormRegistry;

43ebfb981ffec1d4bcde239e832badc2832f4775
Zherro <cleiton.ti@outlook.com>
2021-05-09 18:03:31 -0300

    form stepper

--- src/components/FormRegistry.jsx
+++ src/components/FormRegistry.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect, useState } from 'react';
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
-import { Typography } from '@material-ui/core';
+import { Step, StepLabel, Stepper, Typography } from '@material-ui/core';
 
 function FormRegistry({ validarCPF, aoEnviar }) {
 
@@ -10,13 +10,16 @@ function FormRegistry({ validarCPF, aoEnviar }) {
     const [dadosColetados, setDados] = useState({});
 
     useEffect(() => {
-        console.log(dadosColetados);
+        if (etapaAtual == formularios.length) {
+            console.log(dadosColetados);
+        }
     })
 
     const formularios = [
         <DadosUsuario aoEnviar={coletarDados} />,
         <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
         <DadosEntrega aoEnviar={coletarDados} />,
+        <Typography variant="h5">Obrigado pelo cadastro</Typography>,
     ];
 
     function coletarDados(dados) {
@@ -26,7 +29,15 @@ function FormRegistry({ validarCPF, aoEnviar }) {
     function proximo() {
         setEtapaAtual(etapaAtual + 1);
     }
-    return <>{formularios[etapaAtual]}</>;
+    return <Fragment>
+        <Stepper activeStep={etapaAtual}>
+            <Step><StepLabel>Login</StepLabel></Step>
+            <Step><StepLabel>Pessoal</StepLabel></Step>
+            <Step><StepLabel>Entrega</StepLabel></Step>
+            <Step><StepLabel>Finalização</StepLabel></Step>
+        </Stepper>
+        {formularios[etapaAtual]}
+    </Fragment>;
 }
 
 export default FormRegistry;

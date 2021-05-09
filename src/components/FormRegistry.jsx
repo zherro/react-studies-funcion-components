6844a5dc28eb10f7ae46b1945bdbd33352aad4d3
Zherro <cleiton.ti@outlook.com>
2021-05-09 17:44:43 -0300

    step form

--- src/components/FormRegistry.jsx
+++ src/components/FormRegistry.jsx
@@ -1,97 +1,36 @@
-import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from './DadosUsuario';
+import DadosEntrega from './DadosEntrega';
+import { Typography } from '@material-ui/core';
 
 function FormRegistry({validarCPF, aoEnviar}) {
-    const [nome, setNome] = useState("");
-    const [sobrenome, setSobrenome] = useState("");
-    const [cpf, setCpf] = useState("");
-    const [promocoes, setPromocoes] = useState(true);
-    const [novidades, setNovidades] = useState(false);
 
-    const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
+    const [etapaAtual, setEtapaAtual] = useState(0);
+    const [dadosColetados, setDados] = useState({});
+
+    function formularioAtual(etapa) {
+        switch(etapa) {
+           case 0:
+                return <DadosUsuario aoEnviar={proximo} />;
+            case 1:
+                return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
+            case 2:
+                return <DadosEntrega aoEnviar={aoEnviar}/>;
+            default:
+                return <Typography>Erro ao selecionar formulario</Typography>
+    
+        }
+    }
+
+    function proximo() {
+        setEtapaAtual(etapaAtual + 1);
+    }
 
     return (
-        <form
-            onSubmit={(event) => {
-                event.preventDefault();
-                aoEnviar({ nome, sobrenome, cpf, novidades, promocoes });
-            }}
-        >
-            <TextField
-                value={nome}
-                onChange={(event) => {
-                    setNome(event.target.value);
-                }}
-                id="nome"
-                label="Nome"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-            />
-            <TextField
-                value={sobrenome}
-                onChange={(event) => {
-                    setSobrenome(event.target.value);
-                }}
-                id="sobrenome"
-                label="Sobrenome"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-            />
-            <TextField
-                value={cpf}
-                onChange={(event) => {
-                    setCpf(event.target.value);
-                }}
-
-                onBlur={(event)=>{
-                    const ehValido = validarCPF(cpf);
-                    setErros({cpf:ehValido})
-                  }}
-                
-                error={!erros.cpf.valido}
-                helperText={erros.cpf.texto}
-                
-                id="CPF"
-                label="CPF"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-            />
-
-            <FormControlLabel
-                label="Promoções"
-                control={
-                    <Switch
-                        checked={promocoes}
-                        onChange={(event) => {
-                            setPromocoes(event.target.checked);
-                        }}
-                        name="promocoes"
-                        color="primary"
-                    />
-                }
-            />
-
-            <FormControlLabel
-                label="Novidades"
-                control={
-                    <Switch
-                        checked={novidades}
-                        onChange={(event) => {
-                            setNovidades(event.target.checked);
-                        }}
-                        name="novidades"
-                        color="primary"
-                    />
-                }
-            />
-
-            <Button type="submit" variant="contained" color="primary">
-                Cadastrar
-            </Button>
-        </form >
+        <Fragment>
+            { formularioAtual(etapaAtual) }
+        </Fragment>
     );
 }
 
